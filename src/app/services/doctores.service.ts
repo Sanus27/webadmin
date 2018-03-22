7f891523bd9976e4000a0842063b33055b634e29
Luis Castillo Z <luiscastillo@iwebsapp.com>
2018-03-21 20:50:43 -0600

    Estructura de los doctores

--- src/app/services/doctores.service.ts
+++ src/app/services/doctores.service.ts
@@ -1,15 +1,62 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { AngularFirestore } from 'angularfire2/firestore';
+import { AngularFirestore,  AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
 import { Observable } from 'rxjs/Observable';
 import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from 'firebase/app';
+import { Doctores } from '../models/Doctores';
+
 
 @Injectable()
 export class DoctoresService {
 
-  constructor( public _router:Router, public _db:AngularFirestore, public _auth:AngularFireAuth ) {
+    userscollection: AngularFirestoreCollection<Doctores>;
+    users: Observable<Doctores[]>;
+    userDoc: AngularFirestoreDocument<Doctores>;
+    private resp:string
 
-  }
+    constructor( public _db: AngularFirestore, public _auth:AngularFireAuth ) {
+      this.resp = "success"
+      this.userscollection = this._db.collection('usuarios');
+      this.users = this.userscollection.snapshotChanges().map(
+        changes => { return changes.map( a => {
+            const data = a.payload.doc.data() as Doctores;
+            data.id = a.payload.doc.id;
+            return data;
+        });
+      });
+
+    }
+
+    public getUsers() {
+      return this.users;
+    }
+
+    public createUser(user) {
+      return this._auth.auth.createUserWithEmailAndPassword( user.email, user.password);
+    }
+
+    public addUser(uid, user){
+      return this.userscollection.doc( uid ).set({
+        apellido: user.lastname,
+        nombre: user.name,
+        estado: "0",
+        tipo: "Admin"
+      })
+    }
+
+
+    public deleteUser( user ) {
+      let uid:string = user.id
+      return this._db.collection("usuarios").doc( uid ).delete()
+    }
+
+    public update( id, user ) {
+      return this._db.collection("usuarios").doc( id ).update({
+        apellido: user.lastname,
+        nombre: user.name,
+      })
+    }
+
 
 }
